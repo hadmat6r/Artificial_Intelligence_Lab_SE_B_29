164d8ea87f2eb572b72e22d41b51494a96d8395e
hadmat6r <hadmatsingh.rathod25@mmit.edu.in>
2026-08-06 09:55:51 +0530

    Add files via upload

---
+++ subject.py
@@ -0,0 +1,15 @@
+sub1 = input("enter subject-1  you like: ")
+sub2 = input("enter subject-2  you like: ")
+
+
+if sub1 == "biology" and sub2 == "chemistry":
+	print ("suggested  career path: biotechnology")
+
+elif sub1 == "maths" and sub2 == "physics":
+	print ("suggested career path: mechanical engineering")
+
+elif sub1 == "DSA" and sun2 == "operating system":
+	print ("suggested career path: Data Analysis")
+
+else :
+	print("suggested career path: Explore Subjects")
